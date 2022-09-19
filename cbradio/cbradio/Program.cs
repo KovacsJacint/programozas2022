c0e313cc2cfe5926698ef0193bb93a557a022f09
KovacsJacint <kovacs.jacint.attila.17@ady-nagyatad.hu>
2022-09-19 08:45:42 +0200

    uzenet

---
+++ cbradio/cbradio/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace cbradio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class adatok
+    {
+        public int ora, perc, AdasDb;
+        public string Nev;
+        public adatok()
+        {
+
+        }
+    }
+}
